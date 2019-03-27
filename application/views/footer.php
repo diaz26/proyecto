6214f1621b58e94c2a3340ea60d44d5560d18de0
diaz26 <jeffreydiazaya@outlook.com>
2019-03-27 16:24:59 -0500

    .

---
+++ application/views/footer.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <!-- jQuery -->
+    <script src="<?= base_url(); ?>assets/js/jquery-2.1.1.min.js"></script>
+    <!-- Bootstrap Plugins -->
+    <!--script src="<?= base_url(); ?>assets/js/bootstrap.min.js"></script-->
+    <!-- Template Plugins -->
+    <script src="<?= base_url(); ?>assets/js/jquery.easing.js"></script>
+    <!--script src="<?= base_url(); ?>assets/js/wow.min.js"></script-->
+     <!--script src="<?= base_url(); ?>assets/js/magnific-popup.min.js"></script-->
+    <script src="<?= base_url(); ?>assets/js/jquery.scrollUp.min.js"></script>
+    <script src="<?= base_url(); ?>assets/js/jquery.ajaxchimp.min.js"></script>
+    <!--script src="<?= base_url(); ?>assets/js/slick.min.js"></script-->
+    <!-- Main js -->
+    <!--script src="<?= base_url(); ?>assets/js/main.js"></script-->
+    <!-- Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ application/views/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <!--Metas-->
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="LeadDigital  Landing page Template">
+  <!--Titulo-->
+  <title>BASEO - Buy and Sell Easy Online</title>
+  <!--Css-->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="<?= base_url(); ?>assets/css/bootstrap.min.css">
+	<!--elegant icon font -->
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/css/elegant-icons.css">
+	<!--Animate -->
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/css/animate.css">
+	<!-- Slick -carousel-->
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/css/slick.css">
+	<!-- Magnific Popup-->
+  <link rel="stylesheet" href="<?= base_url(); ?>assets/css/magnific-popup.css">
+	<!--Template Stylesheets css-->
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/css/style.css">
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/css/responsive.css">
+	<!-- Fonts styles -->
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,700,700i" rel="stylesheet">
+	<!-- Favicon -->
+	<link rel="shortcut icon" href="<?= base_url(); ?>images/icon.png" >
+	<link rel="icon" href="<?= base_url(); ?>images/icon.png" >
+  <!--JS-->
+	<script src="<?= base_url(); ?>assets/js/modernizr.js"></script>
+</head>
+<body>
+
+</body>
+</html>
