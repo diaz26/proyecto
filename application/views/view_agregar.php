e2d3da77e81120e052beca48bedda1de0161f025
Pipe1995Garzon <47527537+Pipe1995Garzon@users.noreply.github.com>
2019-05-11 16:21:54 -0500

    Update view_agregar.php
    cambio en agregar productos...

--- application/views/view_agregar.php
+++ application/views/view_agregar.php
@@ -14,41 +14,64 @@
 
           <div class="row hero-padd">
 
-            <div class="col-md-6 col-12 col-sm-6 ml-auto mr-auto">
+          <div class="col-md-6 col-12 col-sm-6 ml-auto mr-auto">
 
-              <div class="hero-text">
+            <div class="hero-text">
 
                 <h2>Agregar productos </h2>
+              <div class="col-md-10 col-12 ml-auto mr-auto">
                 <form action="<?=base_url(); ?>index.php/Cliente/agregardeverdad/"  method="post"  enctype="multipart/form-data">
 
+                <div class="row">
+                    <div class="col-md-4 pr-md-1">
+                      <div class="form-group">
+                        <label><b>Nombre</b></label><br>
+                        <input type="text" class="form-control" name="Nombre" value="">
+                        <input type="hidden" name="id" value="">
+                      </div>
+                </div>
+                <div class="col-md-4 px-md-1">
+                  <div class="form-group">
+                    <label><b>Marca</b></label><br>
+                    <input type="text" class="form-control" name="Marca" value="">
+                  </div>
+                </div>
 
-               <label>Nombre </label> <br>
-               <input type="text" name="Nombre" value=""><br>
-               <label>Marca</label> <br>
-               <input type="text" name="Marca" value=""><br>
-               <label>Descripcion </label> <br>
-               <input type="text" name="Descripcion" value=""><br>
-               <label>Imagen </label> <br>
+                <div class="col-md-4 pl-md-1">
+                  <div class="form-group">
+                    <label><b>Precio</b></label><br>
+                    <input type="number" class="form-control" name="Precio" value="">
+                  </div>
+                </div>
+               <div class="row">
+                 <div class="col-md-5 pr-md-1">
+                   <div class="form-group">
+                     <b>Imagen:</b><br><img src="" width="100%" id="imaproducto">
+                     <input type="file" name="tcarga" id="upload" value="">
+                     <script>
+                     document.getElementById("ver").onchange = function() {
+                       var reader = new FileReader(); //instanciamos el objeto de la api FileReader
 
+                       reader.onload = function(e) {
+                         document.getElementById("imaproducto").src = e.target.result;
+                       };
 
-               <input type="file" name="tcarga" id="upload" value=""><br>
-               <small  >Archivos permitidos (.jpg .png  )</small><br>
-               <script>
-               document.getElementById("ver").onchange = function() {
-                 var reader = new FileReader(); //instanciamos el objeto de la api FileReader
+                       // read the image file as a data URL.
+                       reader.readAsDataURL(this.files[0]);
+                     };
 
-                 reader.onload = function(e) {
-                   document.getElementById("imaproducto").src = e.target.result;
-                 };
-
-                 // read the image file as a data URL.
-                 reader.readAsDataURL(this.files[0]);
-               };
-
-               </script>
-
-               <label>Precio </label> <br>
-               <input type="text" name="Precio" value=""><br><br>
+                     </script>
+                   </div>
+                 </div>
+                 <div class="row">
+                   <div class="col-md-12 pr-md-1">
+                     <div class="form-group">
+                       <label><b>Descripcion:</b></label><br>
+                       <textarea name="Descripcion" class="form-control" rows="2" cols="80"></textarea>
+                     </div>
+                   </div>
+                 </div>
+                 
 
               <input type="submit" value="agregar producto" class="btn btn-info">
               </form>
@@ -57,6 +80,7 @@
 
             </div>
 
+
           </div>
 
         </div>
@@ -64,7 +88,7 @@
       </div>
 
     </div>
-
+  </div>
   </section>
 
   <script src="<?= base_url(); ?>assets/js/jquery-2.1.1.min.js"></script>
